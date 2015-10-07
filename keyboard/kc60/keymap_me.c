1a8d360f223ce351efcfeaff06796a8a0aad9202
TaffyQ <taffy_quinzel@hotmail.com>
2015-10-07 20:41:39 +0200

    basic working layout with switcher

--- keyboard/kc60/keymap_me.c
+++ keyboard/kc60/keymap_me.c
@@ -1,95 +1,94 @@
-#include "keymap_me.h"
+#include "keymap_common.h"
 
-/*
- * SpaceFN
- * http://geekhack.org/index.php?topic=51069.0
- */
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap 0: Default Layer
-   * ,-----------------------------------------------------------.
-   * |Fn1|  &|  [|  {|  }|  (|  =|  *|  )|  +|  ]|  !|  #|Backsp |
-   * |-----------------------------------------------------------|
-   * |Tab  |  ;|  ,|  .|  P|  Y|  F|  G|  C|  R|  L|  /|  @|    \|
-   * |-----------------------------------------------------------|
-   * |Esc   |  A|  O|  E|  U|  I|  D|  H|  T|  N|  S|  -|Return  |
-   * |-----------------------------------------------------------|
-   * |L-Shift |  '|  Q|  J|  K|  X|  B|  M|  W|  V|  Z|R-Shift   |
-   * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |          Space         |Alt |Gui |Fn0 |Ctrl|
-   * `-----------------------------------------------------------'
-   */
-  [0] = {
-    KC_ESC, LSFT(KC_7),KC_LBRC,LSFT(KC_LBRC),LSFT(KC_RBRC),LSFT(KC_9),KC_EQL,LSFT(KC_8),LSFT(KC_0),LSFT(KC_EQL),KC_RBRC,LSFT(KC_1),LSFT(KC_3),KC_BSPC, \
-    KC_TAB, KC_SCLN,KC_COMM,KC_DOT, KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R, KC_L,KC_SLSH,LSFT(KC_2),KC_BSLS, \
-    KC_FN0, KC_A,   KC_O,   KC_E,   KC_U,   KC_I,   KC_D,   KC_H,   KC_T,   KC_N, KC_S, KC_MIN,KC_ENT, \
-    KC_LSFT,KC_QUOT,KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V, KC_Z,       KC_RSFT, \
-    KC_LCTL,KC_LALT,KC_LGUI,          KC_SPC,                       KC_RALT,KC_RGUI,KC_FN1, KC_RCTL},
+    /* 0: dvorak */
+    KEYMAP_ANSI(
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   LBRC,RBRC,BSPC, \
+        TAB, QUOT,COMM,DOT, P,   Y,   F,   G,   C,   R,   L,   SLSH,EQL, BSLS, \
+        ESC, A,   O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,     ENT,  \
+        LSFT,SCLN,Q,   J,   K,   X,   B,   M,   W,   V,   Z,             RSFT, \
+        LCTL,LALT,LGUI,          SPC,                     RALT,RGUI,FN0, RCTL),
+    /* 1: qwerty */
+    KEYMAP_ANSI(
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
+        ESC, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
+        LCTL,LALT,LGUI,          SPC,                     RALT,RGUI,FN0, RCTL),
+    /* 2: Fn0 Layer
+     * ,-----------------------------------------------------------.
+     * |  ~|  %|  7|  5|  3|  1|  9|  0|  2|  4|  6|  8|  `|Delete |
+     * |-----------------------------------------------------------|
+     * |  $  |Vmu|V- |V+ |   |   |   |PgD|PgU|Hom|End|   |FnL|Ins  |
+     * |-----------------------------------------------------------|
+     * |Caps  |Mpp|Mst|Mne|Mpr|   |   |Dow|Up |Lef|Rig|   |        |
+     * |-----------------------------------------------------------|
+     * |        |FnO|FnD|FnI|   |   |   |Pcr|Lck|Bre|   |          |
+     * |-----------------------------------------------------------|
+     * |    |    |    |                        |    |    |    |    |
+     * `-----------------------------------------------------------'
+     *
+     * ,-----------------------------------------------------------.
+     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|       |
+     * |-----------------------------------------------------------|
+     * |     |   | Up|   |   |   |   |   |   |Cal|   |Hom|Ins|FnL  |
+     * |-----------------------------------------------------------|
+     * |      |Lef|Dow|Rig|   |   |Psc|Slk|Pau|   |Tsk|End|        |
+     * |-----------------------------------------------------------|
+     * |        |Del|   |Web|Mut|VoU|VoD|   |PgU|PgD|Del|          |
+     * |-----------------------------------------------------------|
+     * |    |    |    |                        |    |    |    |    |
+     * `-----------------------------------------------------------'
+     * Fn:  to Fn overlay
+     * FnL: to Layout selector overaly
+     * FnO: backlight on/off
+     * FnD: backlight decrease
+     * FnI: backlight increase
+     */
+    KEYMAP_ANSI(
+        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL, \
+        TRNS,MUTE,VOLD,VOLU,TRNS,TRNS,TRNS,PGDN,PGUP,HOME, END,TRNS,FN1, INS, \
+        CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,DOWN,  UP,LEFT,RGHT,TRNS,     TRNS, \
+        TRNS, FN5, FN6, FN7,TRNS,TRNS,TRNS,PSCR,TRNS,TRNS,TRNS,          TRNS, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+    /* 3: Layout selector
+     * ,-----------------------------------------------------------.
+     * | Ld| Lq|   |   |   |   |   |   |   |   |   |   |   |       |
+     * |-----------------------------------------------------------|
+     * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
+     * |-----------------------------------------------------------|
+     * |      |   |   |   |   |   |   |   |   |   |   |   |        |
+     * |-----------------------------------------------------------|
+     * |        |   |   |   |   |   |   |   |   |   |   |          |
+     * |-----------------------------------------------------------|
+     * |    |    |    |                        |    |    |    |    |
+     * `-----------------------------------------------------------'
+     * Ld: set Dvorak layout
+     * Lq: set Qwerty layout
+     */
+    KEYMAP_ANSI(
+         FN2, FN3,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+};
+const uint16_t PROGMEM fn_actions[] = {
+    /* Poker Layout */
+    [0] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
+    [1] = ACTION_LAYER_MOMENTARY(3),  // to Layout selector
+    [2] = ACTION_DEFAULT_LAYER_SET(0),  // set dvorak layout
+    [3] = ACTION_DEFAULT_LAYER_SET(1),  // set qwerty layout
 
-  /* Keymap 1: Fn0 Layer
-   * ,-----------------------------------------------------------.
-   * |  ~|  %|  7|  5|  3|  1|  9|  0|  2|  4|  6|  8|  `|Delete |
-   * |-----------------------------------------------------------|
-   * |  $  |Vmu|V- |V+ |   |   |   |PgD|PgU|Hom|End|   |   |Ins  |
-   * |-----------------------------------------------------------|
-   * |Caps  |Mpp|Mst|Mne|Mpr|   |   |Dow|Up |Lef|Rig|   |        |
-   * |-----------------------------------------------------------|
-   * |        |Bof|Bdo|Bup|   |   |   |Pcr|Lck|Bre|   |          |
-   * |-----------------------------------------------------------|
-   * |    |    |    |                        |    |    |    |    |
-   * `-----------------------------------------------------------'
-   */
-  [1] = {
-    LSFT(KC_GRV), 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
-    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   KC_FN0, \
-    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
-    TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS},
-
-  /* Overlay 1: Fn1
-   * ,-----------------------------------------------------------.
-   * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|       |
-   * |-----------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
-   * |-----------------------------------------------------------|
-   * |      |   |   |   |   |   |   |   |   |   |   |   |Fn2     |
-   * |-----------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |   |   |   |          |
-   * |-----------------------------------------------------------|
-   * |    |    |    |                        |    |    |    |    |
-   * `-----------------------------------------------------------'
-   */
-  [2] = {
-    TRNS,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,TRNS,  \
-    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
-    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   KC_FN2, \
-    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS, \
-    TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS},
-
-  /* Overlay 1: Fn2
-   * ,-----------------------------------------------------------.
-   * |Fn1|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
-   * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  /|  @|    \|
-   * |-----------------------------------------------------------|
-   * |Esc   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
-   * |-----------------------------------------------------------|
-   * |L-Shift |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|R-Shift   |
-   * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |          Space         |Alt |Gui |Fn0 |Ctrl|
-   * `-----------------------------------------------------------'
-   */
-  [3] = {
-    KC_ESC, KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_MIN,KC_EQL,KC_BSPC, \
-    KC_TAB, KC_Q,KC_W,KC_E, KC_R,   KC_T,   KC_Y,   KC_G,   KC_C,   KC_R, KC_L,KC_SLSH,KC_2,KC_BSLS, \
-    KC_FN0, KC_A,   KC_O,   KC_E,   KC_U,   KC_I,   KC_D,   KC_H,   KC_T,   KC_N, KC_S, KC_MIN,KC_ENT, \
-    KC_LSFT,KC_QUOT,KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V, KC_Z,       KC_RSFT, \
-    KC_LCTL,KC_LALT,KC_LGUI,          KC_SPC,                       KC_RALT,KC_RGUI,KC_FN1, KC_RCTL},
+    [4] = ACTION_BACKLIGHT_TOGGLE(),
+    [5] = ACTION_BACKLIGHT_DECREASE(),
+    [6] = ACTION_BACKLIGHT_INCREASE()
 };
 
 /*
  * Fn action definition
  */
-const uint16_t PROGMEM fn_actions[] = {
-  [0] = ACTION_LAYER_TAP_KEY(1, KC_SPACE),
-  [1] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),    // tilde
-};
+/* const uint16_t PROGMEM fn_actions[] = { */
+/*   [0] = ACTION_LAYER_TAP_KEY(1, KC_SPACE), */
+/*   [1] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),    // tilde */
+/* }; */
